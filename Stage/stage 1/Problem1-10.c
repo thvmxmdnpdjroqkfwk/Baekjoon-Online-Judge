d0763cc7b2cc4a65a1a9adb0e3a1847ca5e1271f
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-02-27 13:31:21 +0900

    Add files via upload

---
+++ Stage/stage 1/Problem1-10.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
+	printf("%d\n", (A+B)%C);
+	printf("%d\n", ((A%C)+(B%C))%C);
+	printf("%d\n", (A*B)%C);
+	printf("%d", ((A%C)*(B%C))%C);
+
+	return 0;
+}
