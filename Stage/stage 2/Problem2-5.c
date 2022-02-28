8ec210f3a855989bc33f303d04acee1981a683b0
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-01 00:19:59 +0900

    Add files via upload

---
+++ Stage/stage 2/Problem2-5.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int H, M;
+	scanf("%d %d", &H, &M);
+	if (M >= 45)
+		printf("%d %d", H, M-45);
+	else if (H != 0)
+		printf("%d %d", H-1, M+15);
+	else
+		printf("%d %d", 23, M+15);
+
+	return 0;
+}
