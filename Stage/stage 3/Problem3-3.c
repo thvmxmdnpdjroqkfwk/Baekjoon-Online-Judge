6d0dd81a5197bdd07d56156974098943579665b9
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-02 00:04:33 +0900

    Add files via upload

---
+++ Stage/stage 3/Problem3-3.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n, sum;
+	scanf("%d", &n);
+	sum = 0;
+	for (int i = 1; i <= n; i++)
+		sum = sum + i;
+	printf("%d", sum);
+
+	return 0;
+}
+
+// 1부터 n까지의 합 공식 이용 (위에는 for문 이용)
+/*
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	printf("%d", n*(n+1)/2);
+
+	return 0;
+}
+*/
