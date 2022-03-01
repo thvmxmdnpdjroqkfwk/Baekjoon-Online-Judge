5461474efb9186939eed9937d1f418f6d5615a59
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-02 00:03:24 +0900

    Add files via upload

---
+++ Stage/stage 3/Problem3-1.c
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = 1; i < 10; i++)
+		printf("%d * %d = %d\n", N, i, N*i);
+
+	return 0;
+}
