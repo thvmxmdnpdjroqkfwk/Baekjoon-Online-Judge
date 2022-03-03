b735e3e574ac1d9adb51780a8aab2becf200439e
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-03 10:03:35 +0900

    Add files via upload

---
+++ Stage/stage 3/Problem3-6.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = N; i > 0; i--)
+		printf("%d\n", i);
+
+	return 0;
+}
+
+// i++ 이용 (위에는 i-- 이용)
+/*
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		printf("%d\n", N-i);
+
+	return 0;
+}
+*/
