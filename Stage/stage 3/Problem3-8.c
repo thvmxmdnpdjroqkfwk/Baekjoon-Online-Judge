3e9d27ee50e227f8122124fd56921745e1077dae
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-04 03:27:48 +0900

    Add files via upload

---
+++ Stage/stage 3/Problem3-8.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int T, A, B;
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++)
+	{
+		scanf("%d %d", &A, &B);
+		printf("Case #%d: %d + %d = %d\n", i+1, A, B, A+B);
+	}
+
+	return 0;
+}
