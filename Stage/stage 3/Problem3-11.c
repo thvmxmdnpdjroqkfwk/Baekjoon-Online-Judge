99a31e3ce00e0cb09217dce7d241f71234c0fb03
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-05 01:08:41 +0900

    Add files via upload

---
+++ Stage/stage 3/Problem3-11.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, X, A, i;
+	scanf("%d %d", &N, &X);
+	for (i = 0; i < N; i++)
+	{
+		scanf("%d", &A);
+		if (A < X)
+			printf("%d\n", A);
+	}
+
+	return 0;
+}
+
+/*
+int main()
+{
+	int N, X, i, num;
+	scanf("%d %d", &N, &X);
+	int A[10000];
+	for (i = 0; i < N; i++)
+	{
+		scanf("%d", &num);
+		A[i] = num;
+	}
+	for (i = 0; i < N; i++)
+	{
+		if (A[i] < X)
+			printf("%d ", A[i]);
+	}
+}
+*/
