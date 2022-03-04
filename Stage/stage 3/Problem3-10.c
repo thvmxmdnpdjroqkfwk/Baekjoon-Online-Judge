0cca53464ec0dcdf37d476cb52303eca9690d96f
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-05 01:08:22 +0900

    Add files via upload

---
+++ Stage/stage 3/Problem3-10.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, i, j;
+	scanf("%d", &N);
+	for (i = 1; i <= N; i++)
+	{
+		for (j = 1; j <= N-i; j++)
+			printf(" ");
+		for (j = 1; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
