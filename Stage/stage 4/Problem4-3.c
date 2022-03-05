29702b31e7c0ee0159665d6299879e141f4210fe
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-06 00:08:18 +0900

    Add files via upload

---
+++ Stage/stage 4/Problem4-3.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, new, cycle;
+	scanf("%d", &N);
+	cycle = 0;
+	new = N;
+	while (1)
+	{
+		new = (((new/10)+(new%10))%10)+((new%10)*10);
+		cycle = cycle + 1;
+		if (new == N)
+			break;
+	}
+	printf("%d", cycle);
+
+	return 0;
+}
