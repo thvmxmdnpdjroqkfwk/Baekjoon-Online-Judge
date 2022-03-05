1b14af518f753050ad30d6544092d44a692d2924
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-06 00:07:47 +0900

    Add files via upload

---
+++ Stage/stage 4/Problem4-1.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int A, B;
+	while (1)
+	{
+		scanf("%d %d", &A, &B);
+		if ((A != 0) || (B != 0))
+			printf("%d\n", A + B);
+		else
+			break;
+	}
+
+	return 0;
+}
+
+// 초깃값 선언을 통한 코딩 (위는 아님)
+/*
+int main()
+{
+	int A, B;
+	A = 1; B = 1;
+	while (A+B != 0)
+	{
+		scanf("%d %d", &A, &B);
+		if (A+B != 0)
+			printf("%d\n", A+B);
+	}
+
+	return 0;
+}
+*/
