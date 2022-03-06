9cc4e15e4d3820d97311efa5c7cf2ecfafacdc56
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-07 00:08:24 +0900

    Add files via upload

---
+++ Stage/stage 5/Problem5-3.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main()
+{
+	int A, B, C, n, i;
+	int arr[10] = {0,};
+	scanf("%d %d %d", &A, &B, &C);
+	n = A*B*C;
+	while (n > 0)
+	{
+		arr[n%10] = arr[n%10]+1;
+		n = n/10;
+	}
+	for (i = 0; i < 10; i++)
+		printf("%d\n", arr[i]);
+
+	return 0;
+}
