41aeee5ee22b223a3ded39ef33632e1ec979b795
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-07 00:08:08 +0900

    Add files via upload

---
+++ Stage/stage 5/Problem5-2.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main()
+{
+	int i, input, arr[9], index;
+	int max = 0;
+	for (i = 0; i < 9; i++)
+	{
+		scanf("%d", &input);
+		arr[i] = input;
+		if (input >= max)
+		{
+			max = input;
+			index = i+1;
+		}
+	}
+	printf("%d\n%d", max, index);
+	
+	return 0;
+}
