b52340573f42d1d412849edb71494780f8b1cc25
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-08 00:13:30 +0900

    Add files via upload

---
+++ Stage/stage 5/Problem5-6.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int num, i, j;
+	char check[80];
+	scanf("%d", &num);
+	for (i = 0; i < num; i++)
+	{
+		int score = 0, result = 0;
+		scanf("%s", &check);
+		for (j = 0; j < strlen(check); j++)
+		{
+			if (check[j] == 'O')
+				score++;
+			else
+				score = 0;
+			result = result+score;
+		}
+		printf("%d\n", result);
+	}
+
+	return 0;
+}
