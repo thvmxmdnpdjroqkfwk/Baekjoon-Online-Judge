16097b19cebebe1f9deb951bcfb9503aa77fca0a
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-09 10:19:46 +0900

    Add files via upload

---
+++ Stage/stage 6/Problem6-1.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int sum(int* a, int n);
+
+int main()
+{
+	int n, i, arr[10000];
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	printf("%d\n", sum(arr, n));
+
+	return 0;
+}
+
+int sum(int *a, int n)
+{
+	int i, result = 0;
+	for (i = 0; i < n; i++)
+		result = result+a[i];
+
+	return result;
+}
