2844a91ae2e01569bbc291ce5c4c39ed13226dc4
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-10 00:37:21 +0900

    Add files via upload

---
+++ Stage/stage 7/Problem7-2.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, sum = 0;
+	char num[100];
+	scanf("%d", &N);
+	scanf("%s", &num);
+	for (int i = 0; i < N; i++)
+		sum = sum+(num[i]-48);
+	printf("%d", sum);
+
+}
