bf628c60fad7cd4759e897689d5dcdec78df141a
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-10 00:37:40 +0900

    Add files via upload

---
+++ Stage/stage 7/Problem7-3.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int i, j;
+	char S[100];
+	scanf("%s", S);
+	for (i = 0; i < 26; i++)
+	{
+		int check = 0;
+		for (j = 0; j < strlen(S); j++)
+		{
+			if (S[j] == i+97)
+			{
+				printf("%d ", j);
+				check++;
+				break;
+			}
+		}
+		if (check == 0)
+			printf("%d ", -1);
+	}
+
+	return 0;
+}
