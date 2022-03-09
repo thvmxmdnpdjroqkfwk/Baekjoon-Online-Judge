9621bdb14b3b0186018efb93e1c49fc8334aa594
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-10 00:37:54 +0900

    Add files via upload

---
+++ Stage/stage 7/Problem7-4.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int T, i, j, k, R;
+	char S[20];
+	scanf("%d", &T);
+	for (i = 0; i < T; i++)
+	{
+		scanf("%d %s", &R, &S);
+		for (j = 0; j < strlen(S); j++)
+		{
+			for (k = 0; k < R; k++)
+				printf("%c", S[j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
