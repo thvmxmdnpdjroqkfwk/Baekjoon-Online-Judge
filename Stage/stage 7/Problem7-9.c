017351aa6019444e0fc500c262b227b637ec1161
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-12 01:21:54 +0900

    Add files via upload

---
+++ Stage/stage 7/Problem7-9.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char word[101];
+	scanf("%s", &word);
+	int count = 0, len = strlen(word), i;
+	for (i = 0; i < len; i++)
+	{
+		if (word[i] == 'c' && (word[i+1] == '=' || word[i+1] == '-'))
+		{
+			count++;
+		}
+		else if (word[i] == 'd'&& (word[i+1] == '-' || (word[i+1] == 'z' && word[i+2] == '=')))
+		{
+			count++;
+		}
+		else if ((word[i] == 'l' || word[i] == 'n') && word[i+1] == 'j')
+		{
+			count++;
+		}
+		else if ((word[i] == 's' || word[i] == 'z') && word[i+1] == '=')
+		{
+			count++;
+		}
+	}
+	printf("%d", len-count);
+
+	return 0;
+}
