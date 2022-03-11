e68e69c8349d0e3a0f2bbc6cb26019ffded9f935
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-12 01:22:14 +0900

    Add files via upload

---
+++ Stage/stage 7/Problem7-10.c
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char word[105];
+	int N, i, count = 0;
+	scanf("%d", &N);
+	for (i = 0; i < N; i++)
+	{
+		scanf("%s", &word);
+		int j, len = strlen(word);
+		for (j = 0; j < len; j++)
+		{
+			int check = 0;
+			if (word[j] != word[j+1])
+			{
+				int k;
+				for (k = j + 2; k < len; k++)
+				{
+					if (word[j] == word[k])
+					{
+						count++;
+						check++;
+						break;
+					}
+				}
+			}
+			if (check == 1)
+				break;
+		}
+	}
+	printf("%d", N-count);
+
+	return 0;
+}
