0594abd99339bafe3ede267b8e3c2c15af7ba994
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-13 00:39:54 +0900

    Add files via upload

---
+++ Stage/stage 8/Problem8-2.c
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, i = 2;
+	scanf("%d", &N);
+	if (N == 1)
+		printf("%d", 1);
+	else
+	{
+		while (1)
+		{
+			if ((3*i*i-9*i+7 < N) && (3*i*i-3*i+1 >= N))
+			{
+				printf("%d", i);
+				break;
+			}
+			i++;
+		}
+	}
+
+	return 0;
+}
+
+/*
+int main()
+{
+	int N, i = 1, ans = 1;
+	scanf("%d", &N);
+	while (N > ans)
+	{
+		ans = ans+6*i;
+		i++;
+	}
+	if (N == 1)
+		printf("%d", 1);
+	else
+		printf("%d", i);
+}
+*/
