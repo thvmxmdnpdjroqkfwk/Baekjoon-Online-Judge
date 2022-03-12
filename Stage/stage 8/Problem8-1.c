126842c27f99059bf9bcb36add255d9d7823c290
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-13 00:39:40 +0900

    Add files via upload

---
+++ Stage/stage 8/Problem8-1.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
+	if (C > B)
+		printf("%d", A/(C-B)+1);
+	else
+		printf("%d", -1);
+
+	return 0;
+}
