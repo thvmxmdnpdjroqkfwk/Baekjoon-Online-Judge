7cbe01425dbc4997da08aba3f56aa335abfb12d6
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-13 00:40:07 +0900

    Add files via upload

---
+++ Stage/stage 8/Problem8-3.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int X, i = 1, n = 1;
+	scanf("%d", &X);
+	while (X >= n)
+	{
+		n = n+i;
+		i++;
+	}
+	if (i%2 == 0)
+		printf("%d/%d", n-X, X-(n-i));
+	else
+		printf("%d/%d", X-(n-i), n-X);
+
+	return 0;
+}
