fce0a3dbbe4c2e97b1b5aa3650f3648fcac9cecc
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-14 00:13:31 +0900

    Add files via upload

---
+++ Stage/stage 8/Problem8-4.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+/*
+int main()
+{
+	int position = 0, A, B, V, i = 1;
+	scanf("%d %d %d", &A, &B, &V);
+	while (1)
+	{
+		position = position+A;
+		if (position >= V)
+			break;
+		position = position-B;
+		i++;
+	}
+	printf("%d", i);
+
+	return 0;
+}
+*/
+int main()
+{
+	double A, B, V;
+	scanf("%lf %lf %lf", &A, &B, &V);
+	printf("%.0lf", ceil((V-B)/(A-B)));
+}
