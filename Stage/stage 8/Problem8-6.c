31e77c9022488544eda28278d66465a97d26933a
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-14 00:14:08 +0900

    Add files via upload

---
+++ Stage/stage 8/Problem8-6.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int T;
+	scanf("%d", &T);
+
+	int i, k, n;
+	for (i = 0; i < T; i++)
+	{
+		scanf("%d %d", &k, &n);
+		int num[14] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14 }, j, m;
+		for (j = 0; j < k; j++)
+		{
+			for (m = 1; m < 14; m++)
+			{
+				num[m] = num[m]+num[m-1];
+			}
+		}
+		printf("%d\n", num[n - 1]);
+	}
+
+	return 0;
+}
