c3d2d227b7078b922ad79a04d37b860bbbaddd94
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-15 00:34:16 +0900

    Add files via upload

---
+++ Stage/stage 8/Problem8-7.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	int x = 0, max_x = 0;
+	while (N >= 5*x)
+	{
+		if ((N-5*x)%3 == 0)
+			max_x = x;
+		x++;
+	}
+	if ((N-5*max_x)%3 != 0)
+		printf("%d", -1);
+	else
+		printf("%d", max_x+(N-5*max_x)/3);
+
+	return 0;
+}
