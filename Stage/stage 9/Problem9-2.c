3e9b28f3843d9fa19a0922ce4c9d80da2b052b6d
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-16 01:08:32 +0900

    Add files via upload

---
+++ Stage/stage 9/Problem9-2.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int M, N;
+	scanf("%d %d", &M, &N);
+
+	int i, sum = 0, min = 10000;
+	for (i = M; i <= N; i++)
+	{
+		int j, count = 0;
+		for (j = 2; j <= i; j++)
+		{
+			if (i%j == 0)
+				count++;
+		}
+		if (count == 1)
+		{
+			sum = sum+i;
+			if (min >= i)
+				min = i;
+		}
+	}
+	if (sum > 0)
+		printf("%d\n%d", sum, min);
+	else
+		printf("%d", -1);
+
+	return 0;
+}
