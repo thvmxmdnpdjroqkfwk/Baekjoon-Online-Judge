6466c49ec72bae89ff3b61337c120b26e2865166
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-16 01:09:01 +0900

    Add files via upload

---
+++ Stage/stage 9/Problem9-3.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	if (N != 1)
+	{
+		int div = 2;
+		while (div < N)
+		{
+			if (N%div == 0)
+			{
+				N = N/div;
+				printf("%d\n", div);
+			}
+			else
+				div++;
+		}
+		printf("%d\n", N);
+	}
+
+	return 0;
+}
