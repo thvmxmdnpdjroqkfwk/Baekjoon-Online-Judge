7c6b15a167da4d97f31873c2a073b042e0eceed6
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-16 01:07:52 +0900

    Add files via upload

---
+++ Stage/stage 9/Problem9-1.c
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, i, num, ans = 0;
+	scanf("%d", &N);
+
+	for (i = 0; i < N; i++)
+	{
+		scanf("%d", &num);
+
+		int j, count = 0;
+		if (num != 1)
+		{
+			for (j = 1; j <= num; j++)
+			{
+				if (num%j == 0)
+					count++;
+			}
+			if (count == 2)
+				ans++;
+		}
+	}
+	printf("%d", ans);
+
+	return 0;
+}
