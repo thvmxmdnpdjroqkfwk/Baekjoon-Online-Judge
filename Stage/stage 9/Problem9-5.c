27e06f8ac9810f2e12f60045a3fdfbad10785965
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-17 02:49:43 +0900

    Add files via upload

---
+++ Stage/stage 9/Problem9-5.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n = 1;
+	while (1)
+	{
+		scanf("%d", &n);
+		if (n == 0)
+			break;
+
+		int i, j, arr[246920] = {0,}, count = 0;
+		arr[1] = 1;
+		for (i = 2; i <= n; i++)
+		{
+			for (j = n/i; j <= 2*n/i; j++)
+			{
+				if (j != 1)
+					arr[i*j] = 1;
+			}
+		}
+
+		for (i = n+1; i <= 2*n; i++)
+		{
+			if (arr[i] == 0)
+				count++;
+		}
+		printf("%d\n", count);
+	}
+
+	return 0;
+}
