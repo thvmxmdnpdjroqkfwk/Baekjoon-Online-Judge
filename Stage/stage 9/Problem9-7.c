96e0972b1da3a8041c80e25136f06d1b3243e78f
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-18 01:31:09 +0900

    Add files via upload

---
+++ Stage/stage 9/Problem9-7.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int x, y, w, h;
+	scanf("%d %d %d %d", &x, &y, &w, &h);
+	int arr[5], i;
+	arr[0] = x, arr[1] = y, arr[2] = w-x, arr[3] = h-y;
+	int min = 1000;
+	for (i = 0; i < 4; i++)
+	{
+		if (min > arr[i])
+			min = arr[i];
+	}
+	printf("%d", min);
+
+	return 0;
+}
