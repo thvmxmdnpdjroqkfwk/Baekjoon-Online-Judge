1b7dd5cbfce2e72fc2b1f629426db250e7791e37
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-18 01:31:49 +0900

    Add files via upload

---
+++ Stage/stage 9/Problem9-9.c
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	/*
+	while (1)
+	{
+		int a, b, c;
+		scanf("%d %d %d", &a, &b, &c);
+		if (a == 0 && b == 0 && c == 0)
+			break;
+		if (a*a + b*b == c*c || b*b + c*c == a*a || c*c + a*a == b*b)
+			printf("right\n");
+		else
+			printf("wrong\n");
+	}
+	*/
+	while (1)
+	{
+		int a, b, c, max;
+		scanf("%d %d %d", &a, &b, &c);
+		if (a == 0 && b == 0 && c == 0)
+			break;
+		////// 가장 큰 수를 c로 보내기 //////
+		if (a > b && a > c)
+		{
+			max = a;
+			a = c;
+			c = max;
+		}
+		else if (b > a && b > c)
+		{
+			max = b;
+			b = c;
+			c = max;
+		}
+		///////////////////////////////////
+		if (a*a + b*b == c*c)
+			printf("right\n");
+		else
+			printf("wrong\n");
+	}
+
+	return 0;
+}
