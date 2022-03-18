a5e92d503d5af4cb66f8c271833641be22ecf284
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-19 02:48:40 +0900

    Add files via upload

---
+++ Stage/stage 9/Problem9-10.c
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int R;
+	const double p = 3.14159265359;
+	scanf("%d", &R);
+	printf("%f\n", p*R*R);
+	printf("%f", 2.0*R*R);
+
+	return 0;
+}
