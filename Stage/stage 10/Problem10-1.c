5646d712c48a2b8139017383a66205cb43a93cf9
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-20 01:41:10 +0900

    Add files via upload

---
+++ Stage/stage 10/Problem10-1.c
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int fac(int N)
+{
+	if (N > 1)
+		return N * fac(N - 1);
+	else
+		return 1;
+}
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	printf("%d", fac(N));
+}
+
+/*
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	if (N == 0)
+		printf("%d", 1);
+	else
+	{
+		int i, ans = 1;
+		for (i = 2; i <= N; i++)
+			ans *= i;
+		printf("%d", ans);
+	}
+
+	return 0;
+}
+*/
