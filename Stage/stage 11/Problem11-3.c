c8f008cfb2b81823a6ecda583e8d5a543bb7451e
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-21 00:15:32 +0900

    Add files via upload

---
+++ Stage/stage 11/Problem11-3.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	int i, j, x[50] = { 0, }, y[50] = { 0, };
+	for (i = 0; i < N; i++)
+		scanf("%d %d", &x[i], &y[i]);
+
+	for (i = 0; i < N; i++)
+	{
+		int count = 0;
+		for (j = 0; j < N; j++)
+		{
+			if (x[i] < x[j] && y[i] < y[j])
+				count++;
+		}
+		printf("%d\n", count + 1);
+	}
+
+	return 0;
+}
