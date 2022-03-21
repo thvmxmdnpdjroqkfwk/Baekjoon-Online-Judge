c620a4efbca72f4db33a9a966018f0858534fb9c
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-22 00:03:26 +0900

    Add files via upload

---
+++ Stage/stage 11/Problem11-4.c
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, M;
+	scanf("%d %d", &N, &M);
+
+	int i, j, m, n, min = 50*50;
+	char arr[50][50];
+	for (i = 0; i < N; i++)
+		scanf("%s", &arr[i]);	
+	for (i = 0; i < N - 7; i++)
+	{
+		for (j = 0; j < M - 7; j++)
+		{
+			int cnt1 = 0, cnt2 = 0;
+			for (m = i; m < i + 8; m++)
+			{
+				for (n = j; n < j + 8; n++)
+				{
+					if ((m + n) % 2 == 0)
+					{
+						if (arr[m][n] != 'B')
+							cnt1++;
+						else if (arr[m][n] != 'W')
+							cnt2++;
+					}
+					else
+					{
+						if (arr[m][n] != 'W')
+							cnt1++;
+						else if (arr[m][n] != 'B')
+							cnt2++;
+					}
+				}
+			}
+			if (cnt1 > cnt2)
+			{
+				if (min > cnt2)
+					min = cnt2;
+			}
+			else
+			{
+				if (min > cnt1)
+					min = cnt1;
+			}
+			
+		}
+	}
+	printf("%d", min);
+
+	return 0;
+}
