f8d5351d8684c9b674196e2bb6027ae6413f2bc6
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-23 02:30:29 +0900

    Add files via upload

---
+++ Stage/stage 12/Problem12-2.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b)
+{
+	return *(int*)a - *(int*)b;
+}
+int arr[1000000] = { 0, };
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+	qsort(arr, N, sizeof(int), compare);
+	for (int i = 0; i < N; i++)
+		printf("%d\n", arr[i]);
+}
