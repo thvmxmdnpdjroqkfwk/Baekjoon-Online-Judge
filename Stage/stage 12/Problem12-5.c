2dacf3f3ba306c3cf03723a8176ce268261f6b87
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-24 02:34:03 +0900

    Add files via upload

---
+++ Stage/stage 12/Problem12-5.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main()
+{
+	int N, i, j, arr[10] = { 0, };
+	scanf("%d", &N);
+
+	while (N > 0)
+	{
+		arr[N % 10]++;
+		N /= 10;
+	}
+
+	for (i = 9; i >= 0; i--)
+	{
+		for (j = 0; j < arr[i]; j++)
+			printf("%d", i);
+	}
+
+	return 0;
+}
