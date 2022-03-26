7904a6fb5783b9971f87c5d4975ed8524499b08b
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-27 01:30:34 +0900

    Add files via upload

---
+++ Stage/stage 14/Problem14-4.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int pick[8] = { 0, }, tmp = 0;
+
+void back(int a, int b, int c)
+{
+	int i;
+	if (c == b)
+	{
+		for (i = 0; i < b; i++)
+			printf("%d ", pick[i]);
+		printf("\n");
+	}
+	else
+	{
+		for (i = 1; i <= a; i++)
+		{
+			if (tmp <= i)
+			{
+				pick[c] = i;
+				tmp = i;
+				back(a, b, c + 1);
+				pick[c] = 0;
+				tmp = 0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int N, M;
+	scanf("%d %d", &N, &M);
+	back(N, M, 0);
+
+	return 0;
+}
