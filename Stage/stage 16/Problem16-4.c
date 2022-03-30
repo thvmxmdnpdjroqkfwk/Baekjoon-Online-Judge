664b80b6f5e3cf0c16bcad6f66204d383516d108
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-03-31 01:27:39 +0900

    Add files via upload

---
+++ Stage/stage 16/Problem16-4.c
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char equation[51] = { 0, };
+	scanf("%s", &equation);
+	int i = 0, len = strlen(equation), ans = 0, count = 0, sig = 0, j = 0;
+	while (i < len)
+	{
+		char num[6] = { 0, };
+		while (1)
+		{
+			if (i == len || equation[i] == '-' || equation[i] == '+')
+				break;
+			num[j] = equation[i];
+			i++;
+			j++;
+		}
+		int number = atoi(num);
+		if (sig == 0)
+			ans += number;
+		else
+			ans -= number;
+		if (equation[i] == '+')
+		{
+			if (count == 0)
+				sig = 0;
+			else
+				sig = 1;
+		}
+		else if (equation[i] == '-')
+		{
+			sig = 1;
+			count++;
+		}
+		j = 0;
+		i++;
+	}
+	printf("%d", ans);
+
+	return 0;
+}
