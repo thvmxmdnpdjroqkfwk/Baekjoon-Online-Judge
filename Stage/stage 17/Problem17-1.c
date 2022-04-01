beff9f34bd1b4aae0c18af591b222e9564519dc1
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-01 16:12:55 +0900

    Add files via upload

---
+++ Stage/stage 17/Problem17-1.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int A, B;
+	while (1)
+	{
+		scanf("%d %d", &A, &B);
+		if (A == 0 && B == 0)
+			break;
+		else
+		{
+			if (A % B == 0)
+				printf("multiple\n");
+			else if (B % A == 0)
+				printf("factor\n");
+			else
+				printf("neither\n");
+		}
+	}
+
+	return 0;
+}
