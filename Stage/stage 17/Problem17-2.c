6a83484dfef130735fdb461a3ce0a7f00bb68890
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-01 16:13:17 +0900

    Add files via upload

---
+++ Stage/stage 17/Problem17-2.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, i, arr[50] = { 0, }, max = 1, min = 1000001;
+	scanf("%d", &N);
+	for (i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+	for (i = 0; i < N; i++)
+	{
+		if (max < arr[i])
+			max = arr[i];
+		if (min > arr[i])
+			min = arr[i];
+	}
+	printf("%d", max * min);
+
+	return 0;
+}
