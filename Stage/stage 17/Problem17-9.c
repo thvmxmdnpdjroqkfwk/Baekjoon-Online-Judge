05f319d934aa75559cf0549b3c83774e6dbbdf3b
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-03 01:36:35 +0900

    Add files via upload

---
+++ Stage/stage 17/Problem17-9.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int T, N, M, i, j;
+	scanf("%d", &T);
+	for (i = 0; i < T; i++)
+	{
+		long long ans = 1;
+		scanf("%d %d", &N, &M);
+		if (N > M / 2)
+			N = M - N;
+		for (j = M; j > M - N; j--)
+			ans *= j;
+		for (j = 2; j <= N; j++)
+			ans /= j;
+		printf("%lld\n", ans);
+	}
+
+	return 0;
+}
