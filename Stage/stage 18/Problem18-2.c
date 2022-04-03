e5494dc1d29f405a75f5700f9894142abdb713bb
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-04 02:52:21 +0900

    Add files via upload

---
+++ Stage/stage 18/Problem18-2.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int K, N, i, j = 0, stack[100000] = { 0, }, sum = 0;
+	scanf("%d", &K);
+	for (i = 0; i < K; i++)
+	{
+		scanf("%d", &N);
+		if (N != 0)
+		{
+			stack[j] = N;
+			j++;
+		}
+		else
+		{
+			j--;
+			stack[j] = 0;
+		}
+	}
+	for (i = 0; i < j; i++)
+		sum += stack[i];
+	printf("%d", sum);
+
+	return 0;
+}
