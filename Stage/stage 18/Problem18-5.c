27986be9e9cf842dd074013aa7da3a561d5a8995
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-05 01:33:34 +0900

    Add files via upload

---
+++ Stage/stage 18/Problem18-5.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int n, N, i, j = 0, k = 0, l = 1, stack[100001] = { 0, }, c = 0;
+	char ans[200002] = { 0, };
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &N);
+		while (l <= N)
+		{
+			stack[j] = l;
+			ans[k] = '+';
+			j++;
+			k++;
+			l++;
+		}
+		j--;
+		if (stack[j] != N)
+			c = 1;
+		stack[j] = 0;
+		ans[k] = '-';
+		k++;
+	}
+	if (c == 1)
+		printf("NO");
+	else
+	{
+		int len = strlen(ans);
+		for (i = 0; i < len; i++)
+			printf("%c\n", ans[i]);
+	}
+
+	return 0;
+}
