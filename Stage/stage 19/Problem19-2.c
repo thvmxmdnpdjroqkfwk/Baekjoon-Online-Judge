ad8a55f92736e79044a90abfad04e3eb51c94690
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-06 00:18:10 +0900

    Add files via upload

---
+++ Stage/stage 19/Problem19-2.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int arr[1000000] = { 0, };
+
+int main()
+{
+	int N, i;
+	scanf("%d", &N);
+	for (i = 0; i < N; i++)
+		arr[i] = i + 1;
+	int j = 0, k = N - 1;
+	while (1)
+	{
+		if (j >= k - 1)
+			break;
+		j++;
+		k++;
+		arr[k] = arr[j];
+		j++;
+	}
+	printf("%d", arr[k]);
+
+	return 0;
+}
