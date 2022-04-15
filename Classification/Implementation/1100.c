00a6f52bf4450319ed0b70a874ad816573a81cbd
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-15 23:41:10 +0900

    Add files via upload

---
+++ Classification/Implementation/1100.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int i, j, cnt = 0;
+	for (i = 0; i < 8; i++)
+	{
+		char A[9] = { 0, };
+		scanf("%s", A);
+		for (j = 0; j < 8; j++)
+		{
+			if (A[j] == 'F')
+			{
+				if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1))
+					cnt++;
+			}
+		}
+	}
+	printf("%d", cnt);
+
+	return 0;
+}
+// (i%2==0 && j%2==0) || (i%2==1 && j%2==1) -> 흰색
