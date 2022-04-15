23c64d6103b91ca867c6d3c09219738a817e9fe2
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-15 23:40:47 +0900

    Add files via upload

---
+++ Classification/Implementation/1032.c
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+int main()
+{
+	int N, i, j, len;
+	char S1[51] = { 0, };
+	scanf("%d", &N);
+	for (i = 0; i < N; i++)
+	{
+		char S[51] = { 0, };
+		scanf("%s", S);
+		if (i == 0)
+		{
+			len = strlen(S);
+			for (j = 0; j < len; j++)
+				S1[j] = S[j];
+			continue;
+		}
+		for (j = 0; j < len; j++)
+		{
+			if (S[j] != S1[j])
+				S1[j] = '?';
+		}
+	}
+	printf("%s", S1);
+
+	return 0;
+}
+*/
+int main()
+{
+	int N, i, j, len;
+	char S[51] = { 0, }, S1[51] = { 0, };
+	scanf("%d", &N);
+	for (i = 0; i < N; i++)
+	{
+		if (i == 0)
+		{
+			scanf("%s", S1);
+			len = strlen(S1);
+		}
+		else
+		{
+			scanf("%s", S);
+			for (j = 0; j < len; j++)
+			{
+				if (S[j] != S1[j])
+					S1[j] = '?';
+			}
+		}
+	}
+	printf("%s", S1);
+}
