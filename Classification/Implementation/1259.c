0a4ec4233553439f2f041b7e112d53d5c4c0f0ea
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-15 23:41:30 +0900

    Add files via upload

---
+++ Classification/Implementation/1259.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	for (int i = 0; ; i++)
+	{
+		char S[6] = { 0, };
+		scanf("%s", S);
+		if (S[0] == '0')
+			break;
+		int len = strlen(S), a = 0, b = len - 1, cnt = 0;
+		while (S[a] == S[b] && a < b)
+		{
+			a++;
+			b--;
+			cnt++;
+		}
+		if (cnt == len / 2)
+			printf("yes\n");
+		else
+			printf("no\n");
+	}
+
+	return 0;
+}
