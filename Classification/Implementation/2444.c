93e7540105952b61bf7d4410080681a7dc281164
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-20 01:09:44 +0900

    Add files via upload

---
+++ Classification/Implementation/2444.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, i, j;
+	scanf("%d", &N);
+	for (i = 1; i <= N; i++)
+	{
+		for (j = 1; j <= N - i; j++)
+			printf(" ");
+		for (j = 1; j <= 2 * i - 1; j++)
+			printf("*");
+		printf("\n");
+	}
+	for (i = N - 1; i >= 1; i--)
+	{
+		for (j = 1; j <= N - i; j++)
+			printf(" ");
+		for (j = 1; j <= 2 * i - 1; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
