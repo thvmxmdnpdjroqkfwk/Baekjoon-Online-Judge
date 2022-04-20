24ef3c1a07961211ea92a9817452f3ef1ce13f3d
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-21 01:20:34 +0900

    Add files via upload

---
+++ Classification/Implementation/2523.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, i, j;
+	scanf("%d", &N);
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+	for (i = N - 2; i >= 0; i--)
+	{
+		for (j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
