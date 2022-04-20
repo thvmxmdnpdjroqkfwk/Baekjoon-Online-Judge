dbfb8ce4d4391585349ba61c9ac56f04151f686e
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-21 01:20:02 +0900

    Add files via upload

---
+++ Classification/Implementation/2490.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void result(int x)
+{
+	if (x == 3)
+		printf("A\n");
+	else if (x == 2)
+		printf("B\n");
+	else if (x == 1)
+		printf("C\n");
+	else if (x == 0)
+		printf("D\n");
+	else if (x == 4)
+		printf("E\n");
+}
+
+int main()
+{
+	int i, j, n;
+	for (i = 0; i < 3; i++)
+	{
+		int sum = 0;
+		for (j = 0; j < 4; j++)
+		{
+			scanf("%d", &n);
+			sum += n;
+		}
+		result(sum);
+	}
+}
