b2b7f261a74bb47338f59ad61bae6834f9cb1d5f
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-22 00:39:37 +0900

    Add files via upload

---
+++ Classification/Implementation/2920.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int A[8], i, asc = 0, des = 0;
+	for (i = 0; i < 8; i++)
+	{
+		scanf("%d", &A[i]);
+		if (A[i] == i + 1)
+			asc++;
+		else if (A[i] == 8 - i)
+			des++;
+	}
+	if (asc == 8)
+		printf("ascending");
+	else if (des == 8)
+		printf("descending");
+	else
+		printf("mixed");
+
+	return 0;
+}
