c081747754a9d99869a8768745fbe2ed1e37b654
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-22 00:39:56 +0900

    Add files via upload

---
+++ Classification/Implementation/5338.c
@@ -0,0 +1,9 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	printf("       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|");
+
+	return 0;
+}
