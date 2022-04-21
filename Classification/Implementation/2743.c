ea6a6116d74641d63f6ab4a00c605c5209b0d646
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-22 00:39:23 +0900

    Add files via upload

---
+++ Classification/Implementation/2743.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+/*
+int main()
+{
+	char S[101];
+	scanf("%s", S);
+	printf("%d", strlen(S));
+
+	return 0;
+}
+*/
+int main()
+{
+	char S[101] = { 0, };
+	scanf("%s", S);
+	int i = 0;
+	while (S[i] != 0)
+		i++;
+	printf("%d", i);
+
+	return 0;
+}
