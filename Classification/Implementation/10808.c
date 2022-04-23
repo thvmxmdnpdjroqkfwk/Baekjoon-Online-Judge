818fd085b0c53eccb93770bdb1155adf113d2e74
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-24 02:15:01 +0900

    Add files via upload

---
+++ Classification/Implementation/10808.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char S[101] = { 0, }, ans[26] = { 0, };
+	scanf("%s", S);
+	int i, len = strlen(S);
+	for (i = 0; i < len; i++)
+		ans[S[i] - 'a']++;
+	for (i = 0; i < 26; i++)
+		printf("%d ", ans[i]);
+
+	return 0;
+}
