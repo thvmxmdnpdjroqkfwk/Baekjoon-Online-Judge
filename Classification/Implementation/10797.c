fa53d0596f0793fa5c03db82649f68f7b75f0faa
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-23 15:40:30 +0900

    Add files via upload

---
+++ Classification/Implementation/10797.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, n, i, ans = 0;
+	scanf("%d", &N);
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &n);
+		if (n == N)
+			ans++;
+	}
+	printf("%d", ans);
+
+	return 0;
+}
