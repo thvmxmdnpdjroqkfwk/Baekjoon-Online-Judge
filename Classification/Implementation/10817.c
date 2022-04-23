fd62914a95d05b091a903b161c0521262eb8c917
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-24 02:15:19 +0900

    Add files via upload

---
+++ Classification/Implementation/10817.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int A, B, C, arr[101] = { 0, }, i, cnt = 0;
+	scanf("%d %d %d", &A, &B, &C);
+	arr[A]++;
+	arr[B]++;
+	arr[C]++;
+	for (i = 100; i > 0; i--)
+	{
+		if (arr[i] > 0)
+		{
+			cnt++;
+			arr[i]--;
+			if (cnt == 2)
+			{
+				printf("%d", i);
+				break;
+			}
+			i++;
+		}
+	}
+
+	return 0;
+}
