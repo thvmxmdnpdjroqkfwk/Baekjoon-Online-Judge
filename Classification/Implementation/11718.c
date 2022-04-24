175031cefc9b4eae716f0fbadd70b72ca1818342
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-25 03:19:04 +0900

    Add files via upload

---
+++ Classification/Implementation/11718.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 0; i < 100; i++)
+	{
+		char S[101] = { 0, };
+		gets(S);
+		printf("%s\n", S);
+	}
+	
+	return 0;
+}
