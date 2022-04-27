797dc95e05c795e9009b3574a531799dd20b387d
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-04-28 01:32:36 +0900

    Add files via upload

---
+++ Classification/Mathematics/1094.c
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+int main()
+{
+	int X, small = 64, sum = 64, count = 1;
+	scanf("%d", &X);
+	while (1)
+	{
+		if (sum > X)
+		{
+			small /= 2;
+			if (sum - small >= X)
+				sum -= small;
+			else
+				count++;
+		}
+		else
+			break;
+	}
+	printf("%d", count);
+
+	return 0;
+}
+*/
+// 32 -> 16 16 -> 16 8 -> 16 4 4 -> 16 4 2 2 -> 16 4 2 1
+
+int main()
+{
+	int X, S = 64, count = 0;
+	scanf("%d", &X);
+	while (X > 0)
+	{
+		if (X >= S)
+		{
+			X -= S;
+			count++;
+		}
+		else
+			S /= 2;
+	}
+	printf("%d", count);
+
+	return 0;
+}
