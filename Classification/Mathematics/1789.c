312c06bd7f961f54362c2f3208b78f50f56799fd
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-05-01 01:01:31 +0900

    Add files via upload

---
+++ Classification/Mathematics/1789.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	long long S, i, s = 0;
+	scanf("%lld", &S);
+	for (i = 1; ; i++)
+	{
+		s += i;
+		if (S < s)
+		{
+			printf("%lld", i - 1);
+			break;
+		}
+	}
+
+	return 0;
+}
+// 1~19 190
+// 190~ 19개
+// ~189 18개
