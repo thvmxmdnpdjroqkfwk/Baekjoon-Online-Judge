6949dfbbf645d4e797c4efc81795216fd22dd499
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-05-01 01:01:44 +0900

    Add files via upload

---
+++ Classification/Mathematics/1977.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int M, N, i, sum = 0;
+	scanf("%d %d", &M, &N);
+	M = ceil(sqrt(M));
+	N = floor(sqrt(N));
+	for (i = M; i <= N; i++)
+		sum += i * i;
+	if (sum == 0)
+		printf("-1");
+	else
+		printf("%d\n%d", sum, M * M);
+
+	return 0;
+}
