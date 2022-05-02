0b9eeb7e698e2cc6ed73c306a6d233299376099e
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-05-03 00:41:53 +0900

    Add files via upload

---
+++ Classification/Mathematics/2475.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, i, ans = 0;
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &N);
+		ans += (N * N);
+	}
+	printf("%d", ans % 10);
+
+	return 0;
+}
