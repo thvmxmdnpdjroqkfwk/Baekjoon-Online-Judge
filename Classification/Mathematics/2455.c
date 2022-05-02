70cd701cd5bbe36b9ce940ca2218cba359546c83
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-05-03 00:41:34 +0900

    Add files via upload

---
+++ Classification/Mathematics/2455.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N = 0, A, B, i, max = 0;
+	for (i = 0; i < 4; i++)
+	{
+		scanf("%d %d", &A, &B);
+		N += (B - A);
+		if (max < N)
+			max = N;
+	}
+	printf("%d", max);
+
+	return 0;
+}
+// 빼기 먼저!
