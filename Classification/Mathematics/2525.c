4516b00c5efbdfc3488761c880292c5ee989700a
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-05-04 01:00:44 +0900

    Add files via upload

---
+++ Classification/Mathematics/2525.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
+	int h = C / 60, m = C % 60, H = 0, M;
+	if (B + m >= 60)
+	{
+		M = B + m - 60;
+		H += 1;
+	}
+	else
+		M = B + m;
+	if (A + h + H >= 24)
+		H += A + h - 24;
+	else
+		H += A + h;
+	printf("%d %d", H, M);
+
+	return 0;
+}
