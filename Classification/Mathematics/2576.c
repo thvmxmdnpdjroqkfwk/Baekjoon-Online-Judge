7c7fa2ebeec9fd9b1765e25f01187cb081473bec
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-05-06 01:03:15 +0900

    Add files via upload

---
+++ Classification/Mathematics/2576.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, i, sum = 0, min = 100;
+	for (i = 0; i < 7; i++)
+	{
+		scanf("%d", &N);
+		if (N % 2 == 1)
+		{
+			sum += N;
+			if (min > N)
+				min = N;
+		}
+	}
+	if (sum == 0)
+		printf("-1");
+	else
+		printf("%d\n%d", sum, min);
+
+	return 0;
+}
