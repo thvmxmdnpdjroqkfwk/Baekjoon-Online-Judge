fa24199c0c74b6afc158409fd1f2f97000fc20d3
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-05-06 01:04:02 +0900

    Add files via upload

---
+++ Classification/Mathematics/2747.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	long long pre = 0, now = 1, tmp, n, i;
+	scanf("%lld", &n);
+	for (i = 1; i < n; i++)
+	{
+		tmp = now;
+		now = now + pre;
+		pre = tmp;
+	}
+	printf("%lld", now);
+
+	return 0;
+}
