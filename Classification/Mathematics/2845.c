0339af694b30503b29e1c10cca6ad8aa68b4c5b6
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-05-09 01:23:54 +0900

    Add files via upload

---
+++ Classification/Mathematics/2845.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int L, P, A[6] = { 0, }, i;
+	scanf("%d %d", &L, &P);
+	int N = L * P;
+	for (i = 0; i < 5; i++)
+		scanf("%d", &A[i]);
+	for (i = 0; i < 5; i++)
+		printf("%d ", A[i] - N);
+
+	return 0;
+}
