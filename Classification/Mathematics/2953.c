a3809804d3c6f515ae16203fc6a98062f59446f4
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-05-13 01:26:51 +0900

    Add files via upload

---
+++ Classification/Mathematics/2953.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num, max = 0, i, j;
+	for (i = 1; i < 6; i++)
+	{
+		int P, S = 0;
+		for (j = 0; j < 4; j++)
+		{
+			scanf("%d", &P);
+			S += P;
+			if (S > max)
+			{
+				max = S;
+				num = i;
+			}
+		}
+	}
+	printf("%d %d", num, max);
+
+	return 0;
+}
+
