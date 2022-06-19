009e2432ec82d64a9d1c3e932e731388cc80916a
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-06-19 12:37:26 +0900

    Add files via upload

---
+++ Classification/Mathematics/5543.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int i, A, burger = 2000, drink = 2000;
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &A);
+		if (i < 3 && A < burger)
+			burger = A;
+		else if (i >= 3 && A < drink)
+			drink = A;
+	}
+	printf("%d", burger + drink - 50);
+
+	return 0;
+}
