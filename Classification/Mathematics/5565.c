dd86f78a60e2b236513c7a831d1d85cf7472dd4b
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-06-19 12:37:49 +0900

    Add files via upload

---
+++ Classification/Mathematics/5565.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int P, i, Q;
+	scanf("%d", &P);
+	for (i = 0; i < 9; i++)
+	{
+		scanf("%d", &Q);
+		P -= Q;
+	}
+	printf("%d", P);
+
+	return 0;
+}
