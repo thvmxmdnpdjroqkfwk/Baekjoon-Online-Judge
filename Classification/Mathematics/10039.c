a25f40e47467a39deda67ff8ff8ad5a27bb1766b
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-06-21 15:49:25 +0900

    Add files via upload

---
+++ Classification/Mathematics/10039.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int score, i, ans = 0;
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &score);
+		if (score < 40)
+			score = 40;
+		ans += score;
+	}
+	printf("%d", ans / 5);
+
+	return 0;
+}
