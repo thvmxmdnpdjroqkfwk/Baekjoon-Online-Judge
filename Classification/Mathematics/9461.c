31f5774db665c0bc69cedb963c53fe3b0ac4afea
tltmxparoqkfwk <100254131+tltmxparoqkfwk@users.noreply.github.com>
2022-06-21 15:49:10 +0900

    Add files via upload

---
+++ Classification/Mathematics/9461.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int T, i, j, N;
+	scanf("%d", &T);
+	for (i = 0; i < T; i++)
+	{
+		long long arr[100] = { 1,1,1,2,2, };
+		scanf("%d", &N);
+		for (j = 5; j < N; j++)
+			arr[j] = arr[j - 1] + arr[j - 5];
+		printf("%lld\n", arr[N - 1]);
+	}
+
+	return 0;
+}
